9c3a705c0a64c0280863b9510d525ffd07063b68
easydigs <easydigitalkoncept@gmail.com>
2024-02-02 15:31:02 +0100

    Task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-"""Defines a  function that add two integers"""
+"""Defines a function that add two integers"""
+
+
 def add_integer(a, b=98):
+
     """
     Function that add numbers a and b.
     This function takes two parameters, 'a' and 'b',
     which represent integers or floats.
     If 'a' or 'b' is not an integer or float,
     a TypeError is raised with a corresponding message.
-    
+
     Parameters:
     - a (int or float): The first number.
     - b (int or float): The second number.
-    
+
     Returns:
     int or float: The sum of a and b.
-    
-    
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
