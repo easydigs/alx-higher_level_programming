91f1144c1ac6164921a06bf9b553bf2247b36171
easydigs <easydigitalkoncept@gmail.com>
2024-02-02 13:01:28 +0100

    Task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,11 +2,10 @@
 
 def add_integer(a, b=98):
     """Function that adds 2 integers"""
-    if not a or (type(a) != int and type(a) != float):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
 
-    if type(b) != int and type(b) != float:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    return (int(a) + int(b))
-
+    return int(a) + int(b)
