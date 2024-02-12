14651bdc8a86ab6964197ac0ca0d6866b470f68e
easydigs <easydigitalkoncept@gmail.com>
2024-02-12 19:03:31 +0100

    Square updated

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,7 +25,7 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
-    
+
     def update(self, *args, **kwargs):
         """Update the Square.
         Args:
@@ -51,7 +51,7 @@ class Square(Rectangle):
                 elif a == 3:
                     self.y = arg
                 a += 1
-                
+
         elif kwargs and len(kwargs) != 0:
             for k, v in kwargs.items():
                 if k == "id":
