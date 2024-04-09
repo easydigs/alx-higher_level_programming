7a9fe483bc22c016731eec7a7a7804a58d0e00bd
easydigs <easydigitalkoncept@gmail.com>
2024-04-09 08:29:38 +0100

    Task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// script that prints a square
+
+const argSquare = process.argv;
+const number = parseInt(argSquare[2], 10);
+
+if (argSquare[2]) {
+  if (number) {
+    for (let i = 0; i < number; i++) {
+      let square = '';
+      for (let j = 0; j < number; j++) {
+        square += 'X';
+      }
+      console.log(square);
+    }
+  } else {
+    console.log('Missing size');
+  }
+} else {
+  console.log('Missing size');
+}
