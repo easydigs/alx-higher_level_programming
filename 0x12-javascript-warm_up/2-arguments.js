ebef2e748fe86aaa35c407720fedfe4717bcd6d1
easydigs <easydigitalkoncept@gmail.com>
2024-04-09 07:42:13 +0100

    Task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,3 +2,12 @@
 
 // script that prints a message depending of the number of arguments passed:
 
+const argsNumber = process.argv.length - 2;
+
+if (argsNumber === 0) {
+  console.log('No argument');
+} else if (argsNumber === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
