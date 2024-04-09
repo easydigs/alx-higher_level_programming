b7990e518827edfd239b99da940e05665331d1bb
easydigs <easydigitalkoncept@gmail.com>
2024-04-09 08:07:35 +0100

    Task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// script that prints 3 lines: (like 1-multi_languages.js)
+
+const langsArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+let i = 0;
+
+while (i < langsArray.length) {
+  console.log(langsArray[i]);
+  i++;
+}
