4a402092a11a346c74de60120bb2bc759089fce6
easydigs <easydigitalkoncept@gmail.com>
2024-04-11 11:26:24 +0100

    Task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,34 +2,31 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-	  if (w > 0 && h > 0) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
-	  }
+    }
   }
 
   print () {
-	  let rectChar; // declare var to act as container for printing charcaters
-	  for (let i = 0; i < this.height; i++) {
-      // let the variable be an empty string
+    let rectChar;
+    for (let i = 0; i < this.height; i++) {
       rectChar = '';
       for (let j = 0; j < this.width; j++) {
-		  // append the X char to the variable in each iteration
-		  rectChar += 'X';
+        rectChar += 'X';
       }
       console.log(rectChar);
-	  }
+    }
   }
 
   rotate () {
-	  // had to refactor and use a temp to first hold the width before swapping
-	  const tempSwap = this.width;
-	  this.width = this.height;
-	  this.height = tempSwap;
+    const tempSwap = this.width;
+    this.width = this.height;
+    this.height = tempSwap;
   }
 
   double () {
-	  this.width *= 2;
-	  this.height *= 2;
+    this.width *= 2;
+    this.height *= 2;
   }
 };
