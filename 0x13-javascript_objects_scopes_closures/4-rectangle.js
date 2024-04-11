4ac77af21bcc77b176c6e2d37468a342baa3046b
easydigs <easydigitalkoncept@gmail.com>
2024-04-11 10:09:48 +0100

    Task 4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,32 +1,32 @@
 #!/usr/bin/node
 class Rectangle {
-	constructor (w, h) {
+  constructor (w, h) {
 	  if ((w > 0) && (h > 0)) {
-		this.width = w;
-		this.height = h;
+      this.width = w;
+      this.height = h;
 	  }
-	}
-  
-	print () {
+  }
+
+  print () {
 	  for (let i = 0; i < this.height; i++) {
-		let s = '';
-		for (let j = 0; j < this.width; j++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
 		  s += 'X';
-		}
-		console.log(s);
+      }
+      console.log(s);
 	  }
-	}
-  
-	rotate () {
+  }
+
+  rotate () {
 	  const aux = this.width;
 	  this.width = this.height;
 	  this.height = aux;
-	}
-  
-	double () {
+  }
+
+  double () {
 	  this.width *= 2;
 	  this.height *= 2;
-	}
   }
-  
-  module.exports = Rectangle;
+}
+
+module.exports = Rectangle;
