cacb626cfda70e5c2416f1b9a9cf4a2129b6ab94
easydigs <easydigitalkoncept@gmail.com>
2024-04-28 13:41:43 +0100

    task done

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
-curl -s -L -X PUT -H "Origin: You got me!" -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -sLX PUT -H "Origin:You got me!" -d "user_id=98" 0.0.0.0:5000/catch_me
  
